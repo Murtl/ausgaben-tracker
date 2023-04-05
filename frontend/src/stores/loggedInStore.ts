29512ccb96a492a779414df71cf667bcc5993522
Michael Mertl <Michael.Mertl@hs-augsburg.de>
2023-04-05 19:03:11 +0200

    ATF: loggeInStore hinzugefügt (Pinia)

---
+++ frontend/src/stores/loggedInStore.ts
@@ -0,0 +1,8 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useLoggedInStore = defineStore('loggedInStore', () => {
+    const loggedIn = ref(false)
+
+    return { loggedIn }
+});
