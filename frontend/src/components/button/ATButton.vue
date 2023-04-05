7bff6d1c6c73baead8b3b45b55efb11922d78486
Michael Mertl <Michael.Mertl@hs-augsburg.de>
2023-04-05 15:42:32 +0200

    ATF: ATButton Komponente erstellt (und erstes icon)

---
+++ frontend/src/components/button/ATButton.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+export interface Props {
+  title: string
+  width?: string
+  primary?: boolean
+  secondary?: boolean
+  tertiary?: boolean
+}
+
+withDefaults(defineProps<Props>(), {
+  width: '187px',
+  primary: false,
+  secondary: false,
+  tertiary: false
+})
+
+interface Emits {
+  (e: 'press'): void
+}
+
+const emit = defineEmits<Emits>()
+</script>
+
+<template>
+  <button
+    :class="{
+      primary,
+      secondary,
+      tertiary
+    }"
+    class="at-button-host"
+    @click="emit('press')"
+  >
+<slot name="icon"></slot>
+    {{ title }}
+  </button>
+</template>
+
+<style scoped lang="scss">
+@import 'src/assets/base.scss';
+
+.at-button-host {
+  align-items: center;
+  border: none;
+  border-radius: 5px;
+  box-sizing: border-box;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  user-select: none;
+  font-size: 16px;
+  font-weight: 600;
+  padding: 8px 14px;
+  gap: 4px;
+  width: v-bind(width);
+  height: 36px;
+  margin-bottom: 10px;
+
+  &.primary {
+    background-color: $button-blue;
+    color: $text-white;
+  }
+
+  &.secondary {
+    background-color: $button-grey;
+    color: $text-blue;
+  }
+
+  &.tertiary {
+    background-color: $button-white;
+    color: $text-red;
+  }
+}
+</style>
--- frontend/src/views/ATLogin.vue
+++ frontend/src/views/ATLogin.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
+import ATButtonVue from '@/components/button/ATButton.vue';
+import ATCheckIcon from '@/components/icons/ATCheckIcon.vue';
+
+const handlePress = () => {
+    console.log('pressed');
+}
 </script>
 
 <template>
     <div class="at-login-host">
         <h1>Login</h1>
+        <ATButtonVue title="Login" width="316px" primary @press="handlePress"/>
+        <ATButtonVue title="Neu hier?" width="316px" secondary @press="handlePress"/>
+        <ATButtonVue title="Löschen..." width="316px" tertiary @press="handlePress"/>
+        <ATButtonVue title="Mit Icon" width="316px" primary @press="handlePress">
+            <template #icon>
+                <ATCheckIcon/>
+            </template>
+        </ATButtonVue>
     </div>
 </template>
 
