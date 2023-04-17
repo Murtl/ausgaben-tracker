69a220ebc4d2b48f2c4ee59458c97dc399880ff2
Michael Mertl <Michael.Mertl@hs-augsburg.de>
2023-04-17 17:43:57 +0200

    update project to gitlab version

--- frontend/src/components/icons/ATCornerUpperLeftIcon.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <svg width="25" height="20" viewBox="0 0 20 16" fill="white" xmlns="http://www.w3.org/2000/svg">
-    <path d="M0 16l16-16h-16z"></path>
-  </svg>
-</template>
--- frontend/src/components/icons/ATFilterIcon.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M5 6H19V8L13 13V20L11 18V13L5 8V6Z" fill="#192434" />
-    <path d="M5 4H19V5H5V4Z" fill="#192434" />
-  </svg>
-</template>
